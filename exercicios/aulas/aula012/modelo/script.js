2bd9c5d1a7f41330ae0ec91b70281733592b12c1
joaoedson62 <joaoedsonmachado63@gmail.com>
2022-09-02 17:15:54 -0300

    renomeado as fotos

--- exercicios/aulas/aula012/modelo/script.js
+++ exercicios/aulas/aula012/modelo/script.js
@@ -1,8 +1,9 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+    var img = window.document.getElementById('imagems')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 20
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
