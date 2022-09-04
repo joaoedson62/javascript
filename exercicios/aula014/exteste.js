022620c9ecbb6c354bb8469f5c0e41174cb198aa
joaoedson62 <joaoedsonmachado63@gmail.com>
2022-09-04 13:34:52 -0300

    atualizar

--- exercicios/aula014/exteste.js
+++ exercicios/aula014/exteste.js
@@ -1,9 +1,17 @@
-var c = 1
+console.log('Vai começar...')
+for(var c = 1;c <= 5;c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
+
+
+/*var c = 1
 do {
     console.log(`Passo ${c}`)
     c++
 } while (c<=10)
-
+*/
 
  /*var c = 1
 while (c <= 5) {
