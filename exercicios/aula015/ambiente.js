6cdfb50f09d6bc013d0a12aa8dce588f4318a742
joaoedson62 <joaoedsonmachado63@gmail.com>
2022-09-07 11:39:25 -0300

    Criado a aula 015

---
+++ exercicios/aula015/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1) // Acrecenta um valor no vetor
+num.sort()  // Ordena os numeros no vetor
+console.log(num) // Mostra os números do vetor
+console.log(`Nosso vetor tem ${num.length} posições`)   
+console.log(`O primeiro valor do vetor é ${num[3]}`)
+let pos = num.indexOf(9)
+if (pos == -1){
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor esta na posição ${pos}`)
+}
